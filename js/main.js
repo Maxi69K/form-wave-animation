301733246fab9162429b185b74b60668cf420556
Goran Kekez <gorankekez@gmail.com>
2022-12-06 15:25:10 +0100

    finished app

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Form Wave Animation
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => label.innerHTML = label.innerText.split('').map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`).join(''));
+
+
+
+
+
+
+/*
+<label>
+  <span style="transition-delay: 0ms;">E</span>
+  <span style="transition-delay: 50ms;">m</span>
+  <span style="transition-delay: 100ms;">a</span>
+  <span style="transition-delay: 150ms;">i</span>
+  <span style="transition-delay: 200ms;">l</span>
+</label>
+*/
